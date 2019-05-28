948b935e1bfd4e35cec64f88511493472051ed16
xearthxd <di_stat@hotmail.com>
2019-05-28 20:21:53 +0700

    fix double init from main.py

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def extract_downloaded(file_name):
         subprocess.run(["tar", "-xf", './downloaded/'+file_name, '-C', 'need_to_parse/'])
     
 if __name__ == '__main__':
+    from pymongo import MongoClient
+    DB_NAME_INIT = 'Test'
+    COLLECTION_NAME_INIT = 'S3FilesParseStatus'
+    cnx_INIT = MongoClient("mongodb://tdri:stafftdri@3.0.20.249:27017")
+    con = cnx_INIT[DB_NAME_INIT][COLLECTION_NAME_INIT]
     ACCESS_KEY_ID = 'AKIAJLKZSYZVQZ2I65SA'
     SECRET_ACCESS_KEY = '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'
     REGION_NAME = 'ap-southeast-1'
@@ -55,7 +60,8 @@ if __name__ == '__main__':
     bucket = s3.Bucket('earthlambda')
     
     ## Suppose file name is acquired and store in file_name variable ##
-    file_name = parsers().get_file_name()
+
+    file_name = con.find_one({'parsed': False})['file_name'].split('.')[0]
     
     download_file(file_name)
     print(True)
