e847c2d18ee70af120267a9da97b1c97bb807dcb
tonny <ton_kriddanai@hotmail.com>
2019-06-03 14:07:51 +0700

    remove unused

--- modules/ProvinceMiner.py
+++
@@ -1,7 +0,0 @@
-class ProvinceMiner(object):
-    def __init__(self):
-        print("Initializing Province")
-        pass
-    
-    def mine(self):
-        return {"EN": "Mock Province", "TH": "จังหวัทดสอบ"}
